93a0569c176b9ef7aa3732e811fe9fced28ae647
Angel Simeonov <angel.st.sim@gmail.com>
2023-05-02 15:04:27 +0300

    Basic player gun usage

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    //consider using a bullet scriptable object for different bullet types
+
+    private float _speed = 10f;
+    private float _damage = 1f;
+
+
+    private Rigidbody2D _rb;
+
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 shootDir = mousePos - transform.position;
+        Vector3 rotation = transform.position - mousePos;
+        _rb.velocity = new Vector2(shootDir.x,shootDir.y).normalized * _speed;
+        float rot = Mathf.Atan2(rotation.x, rotation.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot); //rot param expects the bullet to be horizontal pointing right. Adjust angles by adding degrees if sprite is different
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "mob")
+        {
+            //logic to handle hitting a mob
+        }
+
+        if (collision.gameObject.tag == "obstacle")
+        {
+            DestroyBullet();
+        }
+    }
+
+    private void DestroyBullet()
+    {
+        if (gameObject != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -1,15 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class GunController : MonoBehaviour
 {
 
+    [SerializeField] private GameObject bulletPrefab;
+
     private float _rotSpeed = 1500.0f;
     private Transform _containerTransform;
-
     private SpriteRenderer _sr;
+    private Transform _nozzleTransform;
 
     void Start()
     {
@@ -17,6 +16,15 @@ public class GunController : MonoBehaviour
         //We want to move the gun around the parent container so we will consider all transform operations 
         //to be done on the parent
         _containerTransform = transform.parent;
+        _nozzleTransform = GameObject.FindGameObjectWithTag("nozzle").transform;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletPrefab, _nozzleTransform.position, Quaternion.identity);
+        }
     }
 
     void FixedUpdate()
